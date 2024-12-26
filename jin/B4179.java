8abe6e6aaa0f50399fdc7838e50982aeb5746ba6
giljinsu <therin234@gmail.com>
2024-12-26 20:24:20 +0900

    테스트용 커밋

---
+++ jin/B4179.java
@@ -0,0 +1,16 @@
+package jin;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B4179 {
+    static int dx[] = {0,1,0,-1};
+    static int dy[] = {1,0,-1,0};
+    static int graph[][];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
